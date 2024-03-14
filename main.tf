20e3ce4db8c6ccc36653b916bd27f0419b73f8ce
jackjduggan <20094010@mail.wit.ie>
2024-03-14 02:39:37 -0700

    ssh onto bastion working

--- main.tf
+++ main.tf
@@ -68,7 +68,32 @@ resource "aws_security_group" "cc2-terraform-ansible-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "vpc-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 # Step 4: HAProxy Instance
