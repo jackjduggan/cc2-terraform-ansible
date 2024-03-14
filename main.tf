ed261d8320eb450b16dadde851407d4651cb316e
jackjduggan <20094010@mail.wit.ie>
2024-03-14 05:24:32 -0700

    key pair copy to bastion working - first ansible success

--- main.tf
+++ main.tf
@@ -166,5 +166,5 @@ output "haproxy_ip" {
 }
 
 output "bastion_ip" {
-  value = aws_instance.bastion.public_ip
+  value = aws_instance.jump.public_ip
 }
--- update_hosts.sh
+++ update_hosts.sh
@@ -6,6 +6,8 @@ webserver2_ip=$(terraform output -raw webserver2_ip)
 haproxy_ip=$(terraform output -raw haproxy_ip)
 bastion_ip=$(terraform output -raw bastion_ip)
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 # create or overwrite Ansible hosts file
 cat > hosts << EOF
 # Ansible Inventory File
@@ -18,7 +20,7 @@ webserver2 ansible_host=${webserver2_ip}
 haproxy1 ansible_host=${haproxy_ip}
 
 [bastion]
-bastion1 ansible_host=${bastion_ip}
+bastion1 ansible_host=${bastion_ip} ansible_user=jack ansible_ssh_private_key_file=~/.ssh/cc2-tf-ans-key-3.pem
 EOF
 
 echo "Ansible hosts file has been created/updated."
